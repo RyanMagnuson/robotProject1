13703e198298991bb24469376f4419b11df65fc3
ryanmagnuson <prminsd@gmail.com>
2019-09-26 11:47:29 -0700

    dynamic template matching

--- findSoccerBall.py
+++ findSoccerBall.py
@@ -3,31 +3,53 @@ import numpy as np
 import time
 
 
-cap = cv2.VideoCapture(0)
+#in place of making a cascade file
+#would love to replace with cascade when I have the time
+def getBestTemplate(img_gray):
 
-time.sleep(.1)
+	matches = []
+	for x in range(8):
+		template = cv2.imread(('sb%d.png') % x, 0)
+		res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+		threshold = 0.5
+		loc = np.where( res >= threshold)
+		numMatches = loc[0].size
 
-ret, img_rgb = cap.read()
+		print(("Template %d has %d matches") % (x, numMatches))
+		matches.append(numMatches)
+
+	return matches.index(max(matches))
 
 
-#img_rgb = cv2.imread('sBallTest.png')
+def main():
+	cap = cv2.VideoCapture(0)
 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+	#delay for camera to light up else too dark
+	time.sleep(.1)
 
-template = cv2.imread('soccerball5.png', 0)
-w, h = template.shape[::-1]
-res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.5
-loc = np.where( res >= threshold)
+	ret, img_rgb = cap.read()
 
-i = 0
-for pt in zip(*loc[::-1]):
-	i = i + 1
-	cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-print(i)
+	whichTemplate = getBestTemplate(img_gray)
 
-cv2.imshow('Detected',img_rgb)
+	template = cv2.imread(('sb%d.png') % whichTemplate, 0)
+	w, h = template.shape[::-1]
+	res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+	threshold = 0.5
+	loc = np.where( res >= threshold)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	for pt in zip(*loc[::-1]):
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+	cv2.imshow(('Detected with %d') % whichTemplate, img_rgb)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+main()
+
+
+
+
+
+
